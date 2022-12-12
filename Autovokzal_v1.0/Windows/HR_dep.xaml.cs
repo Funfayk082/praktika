7825c19db0b8d676283001627bef43753ba18d1e
Funfayk082 <Funfayk082@gmail.com>
2022-12-13 00:07:33 +0300

    удалил ненужные юзинги

--- Autovokzal_v1.0/Windows/HR_dep.xaml.cs
+++ Autovokzal_v1.0/Windows/HR_dep.xaml.cs
@@ -1,32 +1,11 @@
 ﻿using Autovokzal_v1._0.Models;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using OfficeOpenXml;
-using OfficeOpenXml.FormulaParsing;
 using OfficeOpenXml.Style;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.WebRequestMethods;
 
 namespace Autovokzal_v1._0
 {
