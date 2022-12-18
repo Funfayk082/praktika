55b9e58f5325d74d6dc49c0dca2869c47cc78ec0
Funfayk082 <Funfayk082@gmail.com>
2022-12-18 21:46:25 +0300

    vc

---
+++ Autovokzal_v1.0/Windows/PersSelected.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Autovokzal_v1._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Autovokzal_v1._0.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для PersSelected.xaml
+    /// </summary>
+    public partial class PersSelected : Window
+    {
+        public PersSelected(Personal personal)
+        {
+            this.Title = ("Сотрудник " + personal.Id).ToString();
+            InitializeComponent();
+            PersView(personal);
+        }
+
+        public void PersView(Personal personal)
+        {
+            Short_Id.Text = personal.Short_Id.ToString();
+            Surname.Text = personal.Surname;
+            Name.Text = personal.Name;
+            Patronymic.Text = personal.Patronymic;
+            Date.Text = personal.Date.ToString();
+            Otdel.Text = personal.Otdel;
+            Phone.Text = personal.Phone.ToString();
+        }
+    }
+}
--- Autovokzal_v1.0/obj/Debug/net7.0-windows/Windows/DriversList.g.i.cs
+++ Autovokzal_v1.0/obj/Debug/net7.0-windows/Windows/DriversList.g.i.cs
@@ -61,7 +61,7 @@ namespace Autovokzal_v1._0.Windows {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Autovokzal_v1.0;component/windows/driverslist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Autovokzal_v1.0;V1.0.0.0;component/windows/driverslist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windows\DriversList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
